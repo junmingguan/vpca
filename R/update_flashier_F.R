fd8d92ce540e522d395e42efa3feaaa88a61fa89
junmingguan <260361260@qq.com>
2024-12-05 00:24:57 -0600

    fix on flashier F update

--- R/update_flashier_F.R
+++ R/update_flashier_F.R
@@ -5,20 +5,16 @@
 #' @return flash object with updated EF EF2
 #' @export
 update_flashier_F <- function(flash_obj) {
-  Y <- flash_obj$flashier_fit$Y
-  L <- flash_fit_get_pm(flash_obj$flashier_fit, 1)
-  L2 <- flash_fit_get_p2m(flash_obj$flashier_fit, 1)
-  # flash_obj$L_psd is actually the variance
+  Y <- flash_obj$flash_fit$Y
+  L <- flash_fit_get_pm(flash_obj$flash_fit, 1)
+  L2 <- flash_fit_get_p2m(flash_obj$flash_fit, 1)
   WtW <- t(L) %*% L
   diag(WtW) <- diag(L2)
-  tau <- flash_fit_get_tau(flash_obj$flashier_fit)
+  tau <- flash_fit_get_tau(flash_obj$flash_fit)
   Sigma <- tau * WtW
   diag(Sigma) <- diag(Sigma) + 1
   Sigma <- chol2inv(chol(Sigma))
-  F_pm <- tau * t(Y) %*% W %*% Sigma
-  # F_pm <- tau * Sigma %*% t(W) %*% Y
-  # F_p2m <- F_pm^2 + diag(Sigma)
-  # add diagonal terms to each row of F_pm^2
+  F_pm <- tau * t(Y) %*% L %*% Sigma
   F_p2m <- sweep(F_pm^2, 2, diag(Sigma), "+")
   flash_obj$flash_fit$EF[[2]] <- F_pm
   flash_obj$flash_fit$EF2[[2]] <- F_p2m
