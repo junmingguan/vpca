d79b710d992916cc0c6888f5ecefa2f768843533
junmingguan <260361260@qq.com>
2024-12-10 14:42:35 -0600

    fix diagonal terms of ELtL

--- R/update_flashier_F.R
+++ R/update_flashier_F.R
@@ -9,7 +9,7 @@ update_flashier_F <- function(flash_obj) {
   L <- flash_fit_get_pm(flash_obj$flash_fit, 1)
   L2 <- flash_fit_get_p2m(flash_obj$flash_fit, 1)
   WtW <- t(L) %*% L
-  diag(WtW) <- diag(L2)
+  diag(WtW) <- colSums(L2)
   tau <- flash_fit_get_tau(flash_obj$flash_fit)
   Sigma <- tau * WtW
   diag(Sigma) <- diag(Sigma) + 1
@@ -17,6 +17,7 @@ update_flashier_F <- function(flash_obj) {
   F_pm <- tau * t(Y) %*% L %*% Sigma
   F_p2m <- sweep(F_pm^2, 2, diag(Sigma), "+")
   flash_obj$flash_fit$EF[[2]] <- F_pm
+  flash_obj$F_pm <- F_pm
   flash_obj$flash_fit$EF2[[2]] <- F_p2m
   return(flash_obj)
 }
